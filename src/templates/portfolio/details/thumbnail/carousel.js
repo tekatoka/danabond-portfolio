760a80f68b2a85225419a3ae78eada413bb1c198
tekatoka <dana.b@web.de>
2022-07-18 12:44:27 +0200

    added image carousel

---
+++ src/templates/portfolio/details/thumbnail/carousel.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Carousel from "react-images";
+import PropTypes from "prop-types";
+
+export const CarouselThumb = ({ images, currentImage, onViewChange }) => {
+    return (
+        <Carousel
+            currentIndex={currentImage}
+            hideControlsWhenIdle={false}
+            frameProps={{ autoSize: 'height' }}
+            trackProps={{
+                onViewChange: (idx) => onViewChange(idx),
+                align: 0
+            }}
+            views={images.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title
+            }))}
+        />
+    );
+};
+
+export const CarouselMain = ({ images, currentImage }) => {
+    return (
+        <Carousel
+            currentIndex={currentImage}
+            views={images.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title
+            }))}
+        />
+    );
+};
+
+CarouselThumb.prototype = {
+    images: PropTypes.array.isRequired,
+    currentImage: PropTypes.number.isRequired,
+    onViewChange: PropTypes.func,
+};
+
+CarouselMain.prototype = {
+    images: PropTypes.array.isRequired,
+    currentImage: PropTypes.number.isRequired
+};
--- src/templates/portfolio/details/thumbnail/index.js
+++ src/templates/portfolio/details/thumbnail/index.js
@@ -1,19 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { Modal, ModalGateway } from "react-images";
 import PropTypes from "prop-types";
+import { CarouselThumb, CarouselMain } from './carousel';
 
-const PortfolioDetailsThumb = ({images, title}) => {
+const PortfolioDetailsThumb = ({ images }) => {
+
+    const [sliderImages, setSliderImages] = useState([]);
+    const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+    const openLightbox = () => {
+        setViewerIsOpen(true);
+    }
+
+    const closeLightbox = () => {
+        setCurrentImage(0);
+        setViewerIsOpen(false);
+    };
+    useEffect(() => {
+        let imgArray = [];
+        images && images.map((image, index) => (
+            imgArray = [...imgArray, { src: require('../../../../assets/images/portfolio/' + image) }]
+        ))
+        setSliderImages(imgArray);
+    }, [images])
+
     return (
+
         <div className="portfolio-details-image mb-sm-30 mb-xs-30">
-            {images && images.map((image, index) => (
-                <img key={index} src={require('../../../../assets/images/portfolio/' + image)} alt={title}/>
-            ))}
+            {/* {sliderImages && <Gallery photos={sliderImages} onClick={openLightbox} />} */}
+
+            {sliderImages && sliderImages.length &&
+                <div className='portfolio-carousel-main' style={{ cursor: "pointer" }} onClick={openLightbox}>
+                    <CarouselThumb images={sliderImages} currentImage={currentImage} onViewChange={setCurrentImage} />
+                </div>
+            }
+
+            <ModalGateway>
+                {viewerIsOpen ? (
+                    <Modal onClose={closeLightbox}>
+                        <CarouselMain images={sliderImages} currentImage={currentImage} />
+                    </Modal>
+                ) : null}
+            </ModalGateway>
         </div>
     );
 };
 
 PortfolioDetailsThumb.prototype = {
-    images: PropTypes.array.isRequired,
-    title: PropTypes.string.isRequired,
+    images: PropTypes.array.isRequired
 };
 
 export default PortfolioDetailsThumb;

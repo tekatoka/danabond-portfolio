4e1002ee7abcf43f6c081a043522585eb8da259e
tekatoka <dana.b@web.de>
2022-07-21 11:07:21 +0200

    fixed encoding

--- src/components/emailLink/index.js
+++ src/components/emailLink/index.js
@@ -7,7 +7,7 @@ const encodeBase64 = (input) => {
 
     for (let block = 0, charCode, i = 0, map = chars;
         str.charAt(i | 0) || (map = '=', i % 1);
-        output += map.charAt(63 & block >> 8 - i % 1 * 8)) {
+        output += map.charAt(63 && (block >> 8 - i % 1 * 8))) {
 
         charCode = str.charCodeAt(i += 3 / 4);
 
@@ -15,7 +15,7 @@ const encodeBase64 = (input) => {
             console.log("'btoa' failed: The string to be encoded contains characters outside of the Latin1 range.");
         }
 
-        block = block << 8 | charCode;
+        block = (block << 8) || charCode;
     }
 
     return output;
@@ -34,7 +34,12 @@ const EmailLink = ({ email }) => {
     }
 
     return (
-        <a href={PREFIX + href} onMouseEnter={() => toggleMail(true)} onMouseLeave={() => toggleMail(false)} onClick={() => toggleMail(true)}>{email}</a>
+        <a href={PREFIX + href}
+            onMouseEnter={() => toggleMail(true)}
+            onMouseLeave={() => toggleMail(false)}
+            onClick={() => toggleMail(true)}>
+            {email}
+        </a>
     );
 };
 
